a36500f61a25f13191197d4ac0b8499386f9ac80
João Lopes <joao@inst02.local>
2013-03-03 14:24:25 +0100

    print simulated opening of game

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 #! /usr/bin/env python
 
+import time
 import sys
 from board import HexBoard
 
 
 def main():
     board = HexBoard()
-    board.set(3, 3, "3,3")
+    board.set(0, 0, "wS1")
     print board
+    time.sleep(1)
+    board.set(1, 0, "bS1")
+    print board
+    time.sleep(1)
+    (x, y) = board.get_ll_xy(0, 0)
+    board.set(x, y, "wQ1")
+    print board
+    time.sleep(1)
+    (x, y) = board.get_lr_xy(1, 0)
+    board.set(x, y, "bA1")
+    print board
+    time.sleep(1)
+    import pdb; pdb.set_trace()
+    (x, y) = board.get_ul_xy(0, 0)
+    board.set(x, y, "wS2")
+    print board
+    time.sleep(1)
+    (x, y) = board.get_r_xy(1, 0)
+    board.set(x, y, "bG1")
+    print board
+    time.sleep(1)
 
 
 if __name__ == '__main__':
