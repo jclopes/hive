329bd76884a5c7462229ea317da08d44a264adcb
João Lopes <jc.lopes@gmail.com>
2013-08-24 12:12:18 +0200

    packaging using distutils
    also added creative commons license

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='HiveGame',
+    version='0.1dev',
+    packages=['hivegame',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
